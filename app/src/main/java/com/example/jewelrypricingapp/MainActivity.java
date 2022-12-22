d5936a6a7903d838e0daf85703dcc29028751289
gurt9466 <106596661+gurt9466@users.noreply.github.com>
2022-12-22 14:53:08 +0800

    fix

--- app/src/main/java/com/example/jewelrypricingapp/MainActivity.java
+++ app/src/main/java/com/example/jewelrypricingapp/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.jewelrypricingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+CardView Ring;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Ring = findViewById(R.id.Ring);
+
+        Ring.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,Rlist.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
