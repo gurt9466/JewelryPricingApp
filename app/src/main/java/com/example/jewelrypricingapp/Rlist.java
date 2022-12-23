a36ba17e4f12edf7003412672c71c01fc84af5d1
gurt9466 <106596661+gurt9466@users.noreply.github.com>
2022-12-23 10:43:46 +0800

    update

--- app/src/main/java/com/example/jewelrypricingapp/Rlist.java
+++ app/src/main/java/com/example/jewelrypricingapp/Rlist.java
@@ -1,15 +1,72 @@
 package com.example.jewelrypricingapp;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class Rlist extends AppCompatActivity {
+public class Rlist  extends AppCompatActivity {
 
-
+    ListView listView;
+    String[] mTitle = {"No Stone Ring", "Diamond Ring", "Gem Stone Ring"};
+    int[] images = {R.drawable.subn, R.drawable.subn, R.drawable.subn};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rlist);
+
+        listView = findViewById(R.id.list_view);
+
+        MyAdapter adapter = new MyAdapter(this, mTitle, images);
+        listView.setAdapter(adapter);
+
+           listView.setOnItemClickListener((parent, view, position, id) -> {
+               if(position==0){
+                   startActivity(new Intent(Rlist.this,MainActivity.class));
+               }else if(position==1){
+                   startActivity(new Intent(Rlist.this,MainActivity.class));
+               }else if(position==2){
+                   startActivity(new Intent(Rlist.this,MainActivity.class));
+               }
+           });
+    }
+
+    class MyAdapter extends ArrayAdapter<String> {
+
+        Context context;
+        String[] rTitle;
+        int[] rImgs;
+
+        MyAdapter (Context c, String[] title, int[] imgs) {
+            super(c, R.layout.rlist, R.id.textView1, title);
+            this.context = c;
+            this.rTitle = title;
+            this.rImgs = imgs;
+
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+            LayoutInflater layoutInflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View rlist = layoutInflater.inflate(R.layout.rlist, parent, false);
+            ImageView images = rlist.findViewById(R.id.image);
+            TextView myTitle = rlist.findViewById(R.id.textView1);
+
+            images.setImageResource(rImgs[position]);
+            myTitle.setText(rTitle[position]);
+
+            return rlist;
+        }
     }
 }
